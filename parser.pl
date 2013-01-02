e36424b126431539b8ab17caa9cb0a5ff93a7316
Mathijs Saey <mathsaey@Greyflood.local>
2013-01-03 00:56:15 +0100

    Added a function that can turn a>{b,c}>d into a list [a,[b,c],d]. Also added the > operator

--- parser.pl
+++ parser.pl
@@ -5,6 +5,9 @@
 %:- module(parser, [parseFile/1]).
 :-style_check(-discontiguous).
 
+% Redefine the > operator so that read accepts associative >
+:-op(1,xfy,>).
+
 % Opens the file and invokes the parser
 parseFile(FileName) :- 
 	open(FileName, read, Stream),
@@ -22,17 +25,29 @@ parse(end_of_file, _) 		:- !.
 parse(man = {L}, Stream) 	:- convertArgList(L,X), addMen(X), ! , read(Stream,Term), parse(Term,Stream).
 parse(women = {L}, Stream) 	:- convertArgList(L,X), addWomen(X), ! , read(Stream,Term), parse(Term,Stream).
 
+parse(X : L, Stream) :- convertGTList(L), addPreferences(X,L), read(Stream,Term), parse(Term,Stream).
+
 % Adds all the men/women in a list
 addMen([]).
 addWomen([]).
 addMen([Head|Tail]) 	:- assert(man(Head)), addMen(Tail),!.
 addWomen([Head|Tail]) 	:- assert(women(Head)), addWomen(Tail),!.
 
+addPreferences(X,[]).
 
 %---------------%
 % Extra Clauses %
 %---------------%
 
 % Takes an argument list (e.g. (a, b, c)) and converts it into an actual list [a,b,c]
-convertArgList((Head,Rest), [Head | Tail]) :- convertArgList(Rest, Tail),!.
+convertArgList((Head,Rest), [Head|Tail]) :- convertArgList(Rest, Tail),!.
 convertArgList(Head, [Head]).
+
+%Takes a a>{b,c}>d string and converts it into a list
+convertGTList({Head1,Head2} > Rest, [[Head1,Head2]|Tail]) :- convertGTList(Rest, Tail),!.
+convertGTList(Head > Rest, [Head|Tail]) :- convertGTList(Rest, Tail),!.
+convertGTList(X,[X]).
+
+
+
+
